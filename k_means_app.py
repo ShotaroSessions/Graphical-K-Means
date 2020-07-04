e89ab1d2da7b25d7cf56680b97f464845a086239
Shotaro Sessions <shotarosessions@MacBook-Pro.local>
2020-07-04 15:19:11 -0400

    Organized Functions

--- k_means_app.py
+++ k_means_app.py
@@ -5,7 +5,6 @@ import seaborn as sns
 
 from k_means import (generate_centroids, assign_to_centroids,
                      calculate_centroids, k_means_clusters)
-
 from live_k_means import AnimatedScatter
 
 
@@ -13,7 +12,8 @@ def start_app():
 
     print('Welcome to the K Means App!')
 
-    while True:
+    active = True
+    while active:
 
         file_name = input('Enter path to csv: ')
         while not os.path.isfile(file_name):
@@ -23,44 +23,51 @@ def start_app():
 
         k = int(input('Enter k: '))
 
-        print(df.columns)
+        valid_columns = df.columns
 
-        valid_columns = False
-        while not valid_columns:
-            columns = input('Enter columns to consider. (seperated by spaces):\n').split()
-            valid_columns = True
-            for column in columns:
-                if column not in df.columns:
-                    valid_columns = False
+        print(valid_columns)
+
+        prompt = 'Enter columns to consider. (seperated by spaces):\n'
+        columns = input(prompt).split()
+        while False in [column in df.columns for column in valid_columns]:
+            columns = input(prompt).split()
 
         if len(columns) == 2:
-            centroids = generate_centroids(df, k, columns)
-            groups = assign_to_centroids(df, centroids, columns)
-            groupings = [groups]
-            for count in range(100):
-                if(centroids == calculate_centroids(df, centroids, groups, columns)):
-                    print(f"Finished at PASS: ", count)
-                    break
-                centroids = calculate_centroids(df, centroids, groups, columns)
-                groups = assign_to_centroids(df, centroids, columns)
-                groupings.append(groups)
-
-            x, y = df[columns[0]], df[columns[1]]
-            a = AnimatedScatter(x, y, groupings)
-            plt.show()
+            animated_xy(df, k, columns)
         else:
-            sns.set(style="ticks")
-            groups = k_means_clusters(df, k, columns, 100)
-            df = df[columns].copy()
-            df['groups'] = groups
-            sns.pairplot(df, hue='groups')
-            plt.show()
+            scatter_matrix(df, k, columns)
 
         response = input('Continue plotting?[y/n] ')
         while response not in ['y', 'Y', 'n', 'N']:
             response = input('Continue plotting?[y/n] ')
         if response in ['n', 'N']:
-            break;
+            active = False
+
+
+def animated_xy(df, k, columns):
+    centroids = generate_centroids(df, k, columns)
+    groups = assign_to_centroids(df, centroids, columns)
+    groupings = [groups]
+    for count in range(100):
+        if(centroids == calculate_centroids(df, centroids, groups, columns)):
+            print(f"Finished at PASS: ", count)
+            break
+        centroids = calculate_centroids(df, centroids, groups, columns)
+        groups = assign_to_centroids(df, centroids, columns)
+        groupings.append(groups)
+
+    x, y = df[columns[0]], df[columns[1]]
+    AnimatedScatter(x, y, groupings)
+    plt.show()
+
+
+def scatter_matrix(df, k, columns):
+    sns.set(style="ticks")
+    groups = k_means_clusters(df, k, columns, 100)
+    df = df[columns].copy()
+    df['groups'] = groups
+    sns.pairplot(df, hue='groups')
+    plt.show()
 
 
 if __name__ == '__main__':
